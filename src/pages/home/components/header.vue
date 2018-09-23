1a9605a64ff47bc8ffa37e847bea9564925dd750
helloyong <2628578405@qq.com>
2018-09-23 13:15:00 +0800

    add header

---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="header">
+  <div class="header-left">
+    <span class="iconfont back-icon">&#xe612;</span>
+  </div>
+  <div class="header-input">
+    <span class="iconfont">&#xe66f;</span>输入城市/景点/游玩/主题
+  </div>
+  <div class="header-right">
+    城市
+    <span class="iconfont">&#xe6e9;</span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.header
+  display: flex
+  line-height: .86rem
+  background: $bgColor
+  color: $fff
+  .header-left
+    width: .64rem
+    .back-icon
+      text-align: center
+      font-size: .4rem
+  .header-input
+    flex: 1rem
+    height: .64rem
+    line-height: .64rem
+    margin-top: .12rem
+    margin-left: .2rem
+    padding-left: .2rem
+    background: #fff
+    border-radius: .1rem
+    color: #ccc
+  .header-right
+    width: 1.24rem
+    text-align: center
+</style>
